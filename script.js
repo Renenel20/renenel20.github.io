d62176ce25842bdaba55ff998a25d0c66ea35f4e
Renenel20 <54047473+Renenel20@users.noreply.github.com>
2023-05-29 14:02:10 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var typed = new Typed('h1', {
-        strings: ['<i>I</i> love apples.', 'they are delicious.'],
+        strings: ['<i>I</i> love bananas.', 'they are delicious.'],
         typeSpeed: 50,
         loop: true,
         
